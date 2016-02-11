8e6f6150a7dd2dbee82ebaab511a2e92bc5e38e8
Leyou <leyou@umich.edu>
2016-02-10 22:21:46 -0500

    modified:   Matlab/LatticeDrawing.m

--- Matlab/LatticeDrawing.m
+++ Matlab/LatticeDrawing.m
@@ -1,6 +1,13 @@
-function [ output_args ] = untitled( input_args )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ output_args ] = LatticeDrawing( input_args )
+% A Matlab package for drawing lattice.
+%   Input: [UnitCell,PrimVector]
+%       UnitCell: Atoms positions in a unitcell. Example: [0] for a single atom in a
+%       unitcell.
+%       PrimVector: Primitive vectors for the lattice. Example:[0 1;1 0] for a square lattice.          
+
+%Current goal is to plot the lattice in a figure, label the geometry, label
+%all of the possible bonds (set default level to next nearest unit cells).
+
 
 
 end
